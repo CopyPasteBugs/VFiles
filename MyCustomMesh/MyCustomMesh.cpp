83f42cd986967e3559614ed89697ddc865708f5a
CopyPasteBugs <firstlastmanonmoon@gmail.com>
2017-02-10 05:33:04 +0700

    CustomMesh

---
+++ MyCustomMesh/MyCustomMesh.cpp
@@ -0,0 +1,145 @@
+#include "StdAfx.h"
+#include "GamePlugin.h"
+#include "MyCustomMesh.h"
+
+#include <Cry3DEngine/I3DEngine.h>
+#include <CryRenderer\IRenderer.h>
+#include <Cry3DEngine\IRenderNode.h>
+#include <Cry3DEngine\IIndexedMesh.h>
+
+
+class CCustomMeshRegistrator
+	: public IEntityRegistrator
+{
+	virtual void Register() override
+	{
+		RegisterEntityWithDefaultComponent<CCustomMesh>("CustomMesh", "Defaults", "Particles.bmp");
+	}
+	virtual void Unregister() override
+	{
+
+	}
+public:
+	CCustomMeshRegistrator() {}
+	~CCustomMeshRegistrator()
+	{
+	}
+};
+
+static CCustomMeshRegistrator g_customMeshRegistrator;
+
+CRYREGISTER_CLASS(CCustomMesh)
+
+void CCustomMesh::Initialize()
+{
+	//OnResetState();
+	GetEntity()->Activate(true);
+	material = gEnv->p3DEngine->GetMaterialManager()->LoadMaterial("default.mtl");
+	gEnv->p3DEngine->RegisterEntity(this);
+	
+}
+
+void CCustomMesh::ProcessEvent(SEntityEvent & event)
+{
+	switch (event.event)
+	{
+	case ENTITY_EVENT_XFORM_FINISHED_EDITOR:
+	case ENTITY_EVENT_UPDATE:
+	{
+		matrix = GetEntity()->GetWorldTM();
+		break;
+	}
+	case ENTITY_EVENT_START_LEVEL:
+		OnResetState();
+		break;
+	}
+}
+
+void CCustomMesh::OnShutDown()
+{
+		//m_pSubstitute->Dephysicalize();
+		gEnv->p3DEngine->UnRegisterEntityAsJob(this);
+}
+
+void CCustomMesh::Render(const SRendParams & EntDrawParams, const SRenderingPassInfo & passInfo)
+{
+	// create a temp render params copy to manipulate
+	SRendParams tempParams(EntDrawParams);
+	tempParams.pMaterial = material;
+	tempParams.dwFObjFlags |= FOB_DYNAMIC_OBJECT; // only needed if object moved
+	tempParams.pMatrix = &matrix;
+
+	pStaticObject->Render(tempParams, passInfo);
+}
+
+void CCustomMesh::OnResetState()
+{
+	if (!inited)
+	{		
+		
+		
+		if (!pStaticObject)
+		{
+			pStaticObject = gEnv->p3DEngine->CreateStatObj();
+
+			IIndexedMesh* idxMesh = pStaticObject->GetIndexedMesh();
+			CMesh* mesh = idxMesh->GetMesh();
+			int count = 3;
+			mesh->SetIndexCount(count);
+			mesh->SetVertexCount(count);
+			mesh->SetTexCoordsCount(count);
+			int gotElements = 0;
+			
+			//pos = mesh->GetStreamPtr<Vec3>(CMesh::POSITIONS, &gotElements);
+			
+			pos[0] = Vec3(0, 0, 0);
+			pos[1] = Vec3(0, 100, 0);
+			pos[2] = Vec3(0, 100, 100);
+
+			mesh->SetSharedStream(CMesh::POSITIONS, &pos[0], count);
+
+
+			//uv = mesh->GetStreamPtr<Vec2>(CMesh::TEXCOORDS, &gotElements);
+
+			uv[0] = Vec2(0, 0);
+			uv[1] = Vec2(1, 0);
+			uv[2] = Vec2(1, 1);
+
+			mesh->SetSharedStream(CMesh::TEXCOORDS, &uv[0], count);
+
+
+			//ind = mesh->GetStreamPtr<vtx_idx>(CMesh::INDICES, &gotElements);
+
+			ind[0] = 1;
+			ind[1] = 2;
+			ind[2] = 3;
+
+			mesh->SetSharedStream(CMesh::INDICES, &ind[0], count);
+
+			subset.nNumIndices = count;
+			subset.nNumVerts = count;
+			subset.nMatID = 0;
+			subset.FixRanges(&ind[0]);
+			subset.nFirstVertId = 0;
+			subset.nFirstIndexId = 0;
+			
+			mesh->m_subsets.clear();
+			mesh->m_subsets.push_back(subset);
+
+			mesh->RecomputeGeometricMeanFaceArea();
+
+			bool ret = mesh->Validate(nullptr);
+
+
+			// make the static object update
+			pStaticObject->SetFlags(STATIC_OBJECT_GENERATED | STATIC_OBJECT_DYNAMIC);
+			pStaticObject->Invalidate();
+			pStaticObject->SetMaterial(material);
+
+		}
+		
+		matrix = GetEntity()->GetWorldTM();
+		inited = true;
+	}
+}
+
---
+++ MyCustomMesh/MyCustomMesh.h
@@ -0,0 +1,87 @@
+#pragma once
+#include "StdAfx.h"
+
+#include "GamePlugin.h"
+
+#include <CryEntitySystem/IEntityComponent.h>
+#include <CryEntitySystem/IEntity.h>
+#include <CryEntitySystem/IEntitySystem.h>
+
+#include <Cry3DEngine\IIndexedMesh.h>
+
+class CCustomMesh final
+	: public IEntityComponent
+	, public IRenderNode
+{
+	CRY_ENTITY_COMPONENT_INTERFACE_AND_CLASS(CCustomMesh, "CustomMesh", 0x7C08F4ADDA1D45CC, 0x9DFC5AC3BAF2BDB6);
+
+public:
+	// IEntityComponent
+	virtual void Initialize() override;
+	virtual	void ProcessEvent(SEntityEvent &event) override;
+	virtual uint64 GetEventMask() const override { return BIT64(ENTITY_EVENT_UPDATE) | BIT64(ENTITY_EVENT_START_LEVEL); }
+	//virtual EEntityProxy GetProxyType() const override { return ; };
+	virtual void OnShutDown() override;
+	// IEntityComponent
+
+	//// IEntityPropertyGroup
+	//virtual const char* GetLabel() const override { return "CustomMesh"; };
+	//virtual void SerializeProperties(Serialization::IArchive& archive) override;
+	//// IEntityPropertyGroup
+
+	// *** IRenderNode (override all pure virtual methods)
+	virtual const char* GetName() const override { return "CustomMesh"; };
+	virtual const char* GetEntityClassName() const override { return "CustomMeshClass"; };
+
+	virtual Vec3 GetPos(bool bWorldOnly = true) const override
+	{
+		if (bWorldOnly)
+			return GetEntity()->GetWorldPos();
+		else
+			return GetEntity()->GetPos();
+	};
+	virtual const AABB GetBBox() const override { return AABB(Vec3(-10, -10, -10), Vec3(10, 10, 10)); };
+	virtual void       FillBBox(AABB& aabb) { aabb = GetBBox(); }
+	virtual void       SetBBox(const AABB& WSBBox) override { };
+
+	//! Changes the world coordinates position of this node by delta.
+	//! Don't forget to call this base function when overriding it.
+	virtual void OffsetPosition(const Vec3& delta) {};
+
+	//! Renders node geometry
+	virtual void Render(const struct SRendParams& EntDrawParams, const SRenderingPassInfo& passInfo) override;
+
+	//! Get physical entity.
+	virtual struct IPhysicalEntity* GetPhysics() const override { return GetEntity()->GetPhysics(); };
+	virtual void                    SetPhysics(IPhysicalEntity* pPhys) {};
+
+	//! Set override material for this instance.
+	virtual void SetMaterial(IMaterial* pMat) override { material = pMat; };
+
+	//! Queries override material of this instance.
+	virtual IMaterial* GetMaterial(Vec3* pHitPos = NULL) const override { return material; };
+	virtual IMaterial* GetMaterialOverride() override { return material; };
+
+	virtual float      GetMaxViewDist() override { return 255.0f; };
+	virtual EERType    GetRenderNodeType() override { return EERType::eERType_GeomCache; };
+	virtual void       GetMemoryUsage(ICrySizer* pSizer) const override { pSizer->AddObject(this, sizeof(*this));  };
+
+	// *** IRenderNode
+
+
+	void OnResetState();
+protected:
+	bool inited = false;
+	IRenderNode* pNode = nullptr;
+	IStatObj* pStaticObject = nullptr;
+	Matrix34 matrix;
+
+	IMaterial* material;
+
+	Vec3 pos[3];
+	Vec2 uv[3];
+	vtx_idx ind[3];
+	SMeshSubset subset;
+
+
+};
